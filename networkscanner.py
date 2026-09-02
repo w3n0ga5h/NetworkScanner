d4d565cc4aa3b472a7b8687af384d9cad27729e3
w3n0ga5h <yanis14.sadi@outlook.fr>
2026-09-02 21:48:40 +0100

    finished ARPscan

--- networkscanner.py
+++ networkscanner.py
@@ -6,36 +6,45 @@
 # ]
 # ///
 import scapy.all as scapy
+from subnetcalc import Ip   
 
 
 def main() -> None:
-    test=myscanner("10.15.19.31")
-    test.get_target_ip()
-    test.scanarp()
+    test=myscanner("10.15.19.35/28")
+    test.ipinformation()
+    test.ip_range()
+    #test.scanarp()
 class myscanner:
     def __init__(self, ip):
         # Create member variables
         self.ip = ip
-        self.lenip = len(ip)
+        self.ip_range_list=[]
+        self.instanceip= Ip (self.ip)
+     
+    def ipinformation(self):
+        print(self.instanceip)
 
-    def get_target_ip(self):
-        print(self.ip)
+    def ip_range(self):
+        ip_ranges= self.instanceip.available_ips
+        for a in ip_ranges:
+            self.ip_range_list.append(a)
+            #print(a)
+    
     def scanarp(self):
-        pkt=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst=self.ip)
-        answered_pkt,unanswered_pkt=scapy.srp(pkt,timeout=2)
-        if answered_pkt:
-            print("Received answer")
-            print(len(answered_pkt))
-            queryanswer= (answered_pkt[0][1])
-            print(queryanswer)
-            #print(queryanswer.show()) detail of answer packet
-            print(f"MAC address of {queryanswer.psrc} is {queryanswer.hwsrc}")
-        else:
-            print("No answer received")
+        for x in self.ip_range_list:
+            pkt=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst=x)
+            answered_pkt,unanswered_pkt=scapy.srp(pkt,timeout=1,verbose=0)
+            if answered_pkt:
+                #print("Received answer")
+                #print(len(answered_pkt))
+                queryanswer= (answered_pkt[0][1])
+                #print(queryanswer)
+                #print(queryanswer.show()) #detail of answer packet
+                print(f"MAC address of {queryanswer.psrc} is {queryanswer.hwsrc}")
+            else:
+                #print("No answer received")
+                pass
 
 
 if __name__ == "__main__":
     main()
-
-
-

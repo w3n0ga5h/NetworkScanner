63e2264575bfd266d80a3487df5679f3d47fbb66
w3n0ga5h <yanis14.sadi@outlook.fr>
2026-09-06 18:29:30 +0100

    finalized the V1

--- networkscanner.py
+++ networkscanner.py
@@ -1,20 +1,35 @@
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
+#     "ipaddress>=1.0.23",
 #     "scapy>=2.7.0",
 #     "subnetcalc>=1.0.0",
 # ]
 # ///
 import scapy.all as scapy
 from subnetcalc import Ip   
+import ipaddress
 
 
+
+def ipEntered():
+    while True:
+        try:
+            val = input("Please enter the ip address of the server you wish to connect with:")
+            #print(ipaddress.IPv4Network(val,strict=False))
+            return ipaddress.IPv4Network(val,strict=False)
+        except ValueError:
+            print("Not a valid IP address")
+            
 def main() -> None:
-    test=myscanner("45.33.32.140/29")
-    #test.ipinformation()
+    input_IP=str(ipEntered())
+    test=myscanner(input_IP)
+    test.ipinformation()
     test.ip_range()
-    #test.scanarp()
+    test.scanarp()
     test.stealth_port_scan()
+    test.port_scan()
+    test.ping_sweep()
 
 class myscanner:
     def __init__(self, ip):
@@ -22,7 +37,7 @@ class myscanner:
         self.ip = ip
         self.ip_range_list=[]
         self.instanceip= Ip (self.ip)
-        self.basic_tcp_port_list=[21,22,80,443,445]    
+        self.basic_tcp_port_list=[20,21,22,23,25,53,80,88,115,389,443,445]    
     def ipinformation(self):
         print(self.instanceip)
 
@@ -80,16 +95,57 @@ class myscanner:
                 #print(scan_answer.flags) #detail of answer packet
                     if scan_answer.flags =="SA":
                         print(f"SynAck answer received from port {pkt.dport}")
+                        close_pkt=scapy.IP(dst=self.instanceip.ip,proto=6)/scapy.TCP(dport=each_port,flags="R")
+                        scapy.sr1(close_pkt,timeout=1,verbose=0)
+                #print(f"IP :{queryanswer.psrc} answered and the MAC address is {queryanswer.hwsrc}")
+                else:
+                    print(f"No answer from port {pkt.dport} ")
+        else:
+            for x in self.ip_range_list:
+                print(f"Starting stealth port scan of {x}")
+                for each_port in self.basic_tcp_port_list:
+                    pkt=scapy.IP(dst=self.instanceip.ip,proto=6)/scapy.TCP(dport=each_port,flags="S")
+                    #print(pkt.show())   
+                    answer_pkt=scapy.sr1(pkt,timeout=1,verbose=0)
+                    if answer_pkt:
+                    #print("Received answer")
+                    #print(len(answer_pkt))
+                    #print(answer_pkt)
+                        scan_answer= (answer_pkt[0][1])
+                    #print(scan_answer)
+                    #print(scan_answer.flags) #detail of answer packet
+                        if scan_answer.flags =="SA":
+                            print(f"SynAck answer received from port {pkt.dport}")
+                            close_pkt=scapy.IP(dst=self.instanceip.ip,proto=6)/scapy.TCP(dport=each_port,flags="R")
+                            scapy.sr1(close_pkt,timeout=1,verbose=0)
+                    #print(f"IP :{queryanswer.psrc} answered and the MAC address is {queryanswer.hwsrc}")
+                    else:
+                        print(f"No answer from port {pkt.dport}")
+
+    def port_scan(self):  
+        if self.instanceip.cidr == '32':
+            print(f"Starting port scan of {self.instanceip.ip}")
+            for each_port in self.basic_tcp_port_list:
+                pkt=scapy.IP(dst=self.instanceip.ip,proto=6)/scapy.TCP(dport=each_port,flags="S")
+                #print(pkt.show())   
+                answer_pkt=scapy.sr1(pkt,timeout=1,verbose=0)
+                if answer_pkt:
+                #print("Received answer")
+                #print(len(answer_pkt))
+                #print(answer_pkt)
+                    scan_answer= (answer_pkt[0][1])
+                #print(scan_answer)
+                #print(scan_answer.flags) #detail of answer packet
+                    if scan_answer.flags =="SA":
+                        print(f"SynAck answer received from port {pkt.dport}")
                         close_pkt=scapy.IP(dst=self.instanceip.ip,proto=6)/scapy.TCP(dport=each_port,flags="A")
                         scapy.sr1(close_pkt,timeout=1,verbose=0)
                 #print(f"IP :{queryanswer.psrc} answered and the MAC address is {queryanswer.hwsrc}")
                 else:
-                    print("No received answer")
+                    print(f"No answer from port {pkt.dport}")
         else:
-            print(self.ip_range_list)
             for x in self.ip_range_list:
-                print("test")
-                print(f"Starting stealth port scan of {x}")
+                print(f"Starting port scan of {x}")
                 for each_port in self.basic_tcp_port_list:
                     pkt=scapy.IP(dst=self.instanceip.ip,proto=6)/scapy.TCP(dport=each_port,flags="S")
                     #print(pkt.show())   
@@ -107,8 +163,37 @@ class myscanner:
                             scapy.sr1(close_pkt,timeout=1,verbose=0)
                     #print(f"IP :{queryanswer.psrc} answered and the MAC address is {queryanswer.hwsrc}")
                     else:
-                        print("No received answer")
-    def port_scan(self):
-        pass
+                        print(f"No answer from port {pkt.dport}")
+    def ping_sweep(self):
+        print("Starting ping sweep:")
+        if self.instanceip.cidr == '32':
+            pkt=scapy.IP(dst=self.instanceip.ip)/scapy.ICMP()
+            #pkt.show()
+            answered_pkt=scapy.sr1(pkt,timeout=64,verbose=0,retry=4)
+            if answered_pkt:
+                print(f"Received answer from {self.instanceip.ip}")
+                #print(len(answered_pkt))
+                #queryanswer= (answered_pkt[0][1])
+                #print(queryanswer) 
+                #print(queryanswer.show()) #detail of answer packet
+                #print(f"IP :{queryanswer.psrc} answered and the MAC address is {queryanswer.hwsrc}")
+            else:
+                print("No answer received")
+                pass            
+        else:
+            for x in self.ip_range_list:
+                print(f"trying to ping : {x}")
+                pkt=scapy.IP(dst=x)/scapy.ICMP()
+                answered_pkt,unanswered_pkt=scapy.srp(pkt,timeout=4,verbose=0)
+                if answered_pkt:
+                    print(f"Received answer from {x}")
+                    #print(len(answered_pkt))
+                    #queryanswer= (answered_pkt[0][1])
+                    #print(queryanswer)
+                    #print(queryanswer.show()) #detail of answer packet
+                    #print(f"IP :{queryanswer.psrc} answered and the MAC address is {queryanswer.hwsrc}")
+                else:
+                    #print("No answer received")
+                    pass
 if __name__ == "__main__":
     main()
